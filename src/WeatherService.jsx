32dab3bb8ec351a3821946370be4d29aa5d78a89
fatiha zaz <93740626+fatihaZaaz@users.noreply.github.com>
2023-01-11 18:46:25 +0100

    Add files via upload

---
+++ src/WeatherService.jsx
@@ -0,0 +1,38 @@
+const API_KEY = "f108f4adb253faa49fe23d5f3cbdf504";
+
+const makeIconURL = (iconId) =>
+  `https://openweathermap.org/img/wn/${iconId}@2x.png`;
+
+const getFormattedWeatherData = async (city, units = "metric") => {
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=81fe5bc3071f63085b08e523e60f39b8&units=${units}`;
+
+  const data = await fetch(URL)
+    .then((res) => res.json())
+    .then((data) => data);
+
+  const {
+    weather,
+    main: { temp, feels_like, temp_min, temp_max, pressure, humidity },
+    wind: { speed },
+    sys: { country },
+    name,
+  } = data;
+
+  const { description, icon } = weather[0];
+
+  return {
+    description,
+    iconURL: makeIconURL(icon),
+    temp,
+    feels_like,
+    temp_min,
+    temp_max,
+    pressure,
+    humidity,
+    speed,
+    country,
+    name,
+  };
+};
+
+export { getFormattedWeatherData };

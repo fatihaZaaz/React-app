7139661227963661818a0f09db7076b99d53abd2
fatiha zaz <93740626+fatihaZaaz@users.noreply.github.com>
2023-01-11 18:36:45 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Translate from './pages/Translate';
+import Weather from './components/Weather';
+import WeatherApp from './pages/WeatherApp';
+
+function App() {
+  return (
+    <div>
+      <WeatherApp />
+    </div>
+  );
+}
+
+export default App;
